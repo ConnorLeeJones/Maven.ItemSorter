aa529297dcfc6a78c57a6ea3798f33528ec7b5c3
Connor Jones <ConnorLeeJones@gmail.com>
2019-06-07 13:20:08 -0400

    Done

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,25 @@
 package rocks.zipcode.io;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        ArrayList<Item> itemsList = new ArrayList<Item>(Arrays.asList(items));
+        Collections.sort(itemsList, comparator);
+        items = new Item[itemsList.size()];
+        items = itemsList.toArray(items);
+        return items;
     }
 }
